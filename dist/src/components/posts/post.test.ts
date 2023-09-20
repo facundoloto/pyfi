1fd7159dd7a04fac1df28ac9dac55398d81afb6d
ema <lotofacundo6@gmail.com>
2023-09-20 12:50:41 -0300

    build project

--- dist/src/components/posts/post.test.ts
+++ dist/src/components/posts/post.test.ts
@@ -1,37 +1,37 @@
-import { expect } from 'chai';
-import request from 'supertest';
-import app from '../../../index';
+// import { expect } from 'chai';
+// import request from 'supertest';
+// import app from '../../../index';
 
-let token: string = "";
+// let token: string = "";
 
-describe('prove it if the user can access to all post with jwt ', () => {
-    it('should return token by login', async () => {
+// describe('prove it if the user can access to all post with jwt ', () => {
+//     it('should return token by login', async () => {
 
-        const loginData = {
-            email: 'fotofacundo6@gmail.com',
-            password: 'facundo97'
-        };
+//         const loginData = {
+//             email: 'fotofacundo6@gmail.com',
+//             password: 'facundo97'
+//         };
 
-        const response = await request(app)
-            .post('/v1/register/login')
-            .send(loginData);
+//         const response = await request(app)
+//             .post('/v1/register/login')
+//             .send(loginData);
 
-        // Assertions using Chai
-        expect(response.status).to.equal(200);
-        token = response.body.token;
-    });
+//         // Assertions using Chai
+//         expect(response.status).to.equal(200);
+//         token = response.body.token;
+//     });
 
-    it('should return all posts by user', async () => {
-        const loginData = {
-            email: 'user@example.com',
-            password: 'wrongpassword'
-        };
+//     it('should return all posts by user', async () => {
+//         const loginData = {
+//             email: 'user@example.com',
+//             password: 'wrongpassword'
+//         };
 
-        const response = await request(app)
-            .post('/v1/register/login')
-            .send(loginData);
+//         const response = await request(app)
+//             .post('/v1/register/login')
+//             .send(loginData);
 
-        // Assertions using Chai
-        expect(response.status).to.equal(404);
-    });
-});
+//         // Assertions using Chai
+//         expect(response.status).to.equal(404);
+//     });
+// });
