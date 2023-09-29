467500a281360ffbaaa7d9441945153adef9cff4
ema <lotofacundo6@gmail.com>
2023-09-29 01:21:38 -0300

    fix cors

--- dist/index.ts
+++ dist/index.ts
@@ -9,13 +9,13 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 const server = http.createServer(app);
 //const io = new Server(server, { cors: {origin: "*"},});
-const corsOptions = {
-  origin: "https://instagrampyfi.netlify.app",
-  credentials: true,
-}
+// const corsOptions = {
+//   origin: "https://instagrampyfi.netlify.app",
+//   credentials: true,
+// }
 
 app.use(cookieParser());
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set('view engine', '.hbs');
