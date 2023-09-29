533f0c67d5de773f759a84ca39d1dcc67386c97b
ema <lotofacundo6@gmail.com>
2023-09-29 01:39:23 -0300

    aws test

--- dist/src/services/aws/uploadImageServices.ts
+++ dist/src/services/aws/uploadImageServices.ts
@@ -3,17 +3,17 @@ import multerS3 = require('multer-s3');
 import { S3Client } from '@aws-sdk/client-s3';
 require('dotenv').config();
 
-const bucketName : string  = process.env.AWS_BUCKET as string;
-const accessKeyId : string  = process.env.AWS_ACCESS_KEY_ID as string;
-const secretAccesKey : string  = process.env.AWS_SECRET_ACCESS_KEY as string;
-const regionAws: string = process.env.AWS_REGION as string;
+const bucketName: string = process.env.AWS_BUCKET as string;
+const accessKeyId: string = process.env.AWS_ACCESS_KEY_ID as string;
+const secretAccesKey: string = process.env.AWS_SECRET_ACCESS_KEY as string;
+// const regionAws: string = process.env.AWS_REGION as string;
 
 const s3Config = new S3Client({
-  region: regionAws,
-  credentials:{
-     accessKeyId:accessKeyId,
-     secretAccessKey:secretAccesKey
- }
+  // region: regionAws,
+  credentials: {
+    accessKeyId: accessKeyId,
+    secretAccessKey: secretAccesKey
+  }
 });
 
 /**
@@ -22,33 +22,33 @@ const s3Config = new S3Client({
  */
 const imageFilter = (_req: any, file: any, cb: any) => {
 
-    if (!file.originalname.match(/\.(JPG|jpg|jpeg|png|gif)$/)) {
-        return cb(new Error('Only image files are allowed!'), false);
-    }
+  if (!file.originalname.match(/\.(JPG|jpg|jpeg|png|gif)$/)) {
+    return cb(new Error('Only image files are allowed!'), false);
+  }
 
-    cb(null, true);
+  cb(null, true);
 };
 
 /**
  * function to upload your image in aws.
  */
 export const uploadImage = multer({
-    fileFilter: imageFilter,
-    storage: multerS3({
+  fileFilter: imageFilter,
+  storage: multerS3({
 
-        s3:s3Config,
-        bucket: `${bucketName}`,
+    s3: s3Config,
+    bucket: `${bucketName}`,
 
-        metadata: (_req:any, file:any, cb:any) => {
-          cb(null, {fieldName: file.fieldname});
-        },
-        key: (_req:any, _file:any, cb:any) => {
-          cb(null, `${Date.now().toString()}.jpg`)
-        },
-      }),
-      limits: {
-        fileSize: 1024 * 1024 * 10 // 2mb file size
-    }
+    metadata: (_req: any, file: any, cb: any) => {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: (_req: any, _file: any, cb: any) => {
+      cb(null, `${Date.now().toString()}.jpg`)
+    },
+  }),
+  limits: {
+    fileSize: 1024 * 1024 * 10 // 2mb file size
+  }
 }).single('image');
 
 module.exports = { uploadImage };
