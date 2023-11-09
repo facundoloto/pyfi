7270088a6a8d5e23a1fd14be76575d547a9b15da
Facundo_Loto <55114127+facundoloto@users.noreply.github.com>
2023-11-09 10:12:31 -0300

    Update controller.ts

--- dist/src/components/loggin/controller.ts
+++ dist/src/components/loggin/controller.ts
@@ -2,19 +2,15 @@ import { Request, Response } from 'express';
 import { checkEmail, comparePassword } from '../../utils/verify';
 import { HttpStatusCode, responseHttp } from './../../constant/httpCodes';
 import { LoginInterfaces, UserInterfaces } from '../../interfaces/interfaces';
-
 import { userDto, loginDto } from '../users/userDto';
 import setUserDto from '../users/setUserDto';
 import UserDao from '../users/services';
 import Auth from '../auth/auth';
 
-
-
 const userDao = new UserDao();
 const auth = new Auth();
 
 export class LoginController {
-
     async signUp(req: Request, res: Response) {
         const user = await setUserDto(req, res);
         const findEmail = await checkEmail(req.body.email);
@@ -29,43 +25,35 @@ export class LoginController {
             let responseOk: responseHttp = { status: true, result: result };
             return responseOk;
         }
+        
     };
 
-
     async signUpGoogle(req: Request, _res: Response) {
-
         const user: UserInterfaces = {
             name: req.body.name,
             email: req.body.email,
             password: "",
             image_user: req.body.image
         };
-
         const result = await userDao.save(user);
-
         const token = auth.generateToken(result.id);
-
         _res.json({ data: result, result: "access succeful", token: token }).status(HttpStatusCode.Ok);
     };
 
     /*only in this controller we won't use the utils try y catch 'cause we're going to give a cookie to client*/
     async Login(req: Request, _res: Response) {
-
         const login: LoginInterfaces = { email: req.body.email, password: req.body.password };
-
         const findEmail: boolean = await checkEmail(login.email);
         const user: loginDto | any = await userDao.findByEmail(login.email);
 
         if (findEmail) {
             const findPassword: boolean = await comparePassword(login.password, user.password)
-
             if (findPassword) {
                 const userDto: userDto = await userDao.findById(user.id);
                 /*id only is the sign in the token*/
                 const token = auth.generateToken(userDto.id);
                 /*I do this destructuring and spread because the Id is being sent by cookie*/
                 const data = userDto;
-
                 _res.json({ data: data, result: "access succeful", token: token }).status(HttpStatusCode.Ok);
             }
             else {
@@ -77,9 +65,9 @@ export class LoginController {
 
     };
 
-
     async LoginByGoogle(req: Request, _res: Response) {
         const user: userDto | any = await userDao.findByEmail(req.body.email);
+
         if (user) {
             /*id only is the sign in the token*/
             const token = auth.generateToken(user.id);
@@ -90,5 +78,7 @@ export class LoginController {
             _res.json({ result: "email not found" }).status(HttpStatusCode.Unauthorized);
 
         }
+        
     };
+    
 }
