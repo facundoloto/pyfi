1603e399ceef0ed49729173da1c100afc64fb27f
Facundo_Loto <55114127+facundoloto@users.noreply.github.com>
2024-03-20 19:35:48 -0300

    Update index.ts

--- dist/index.ts
+++ dist/index.ts
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 const server = http.createServer(app);
 const corsOptions = {
-  origin: "https://pifyfrontend.netlify.app/login",
+  origin: "https://pifyfrontend.netlify.app",
   // origin: "http://localhost:5173",
   credentials: true,
 }
